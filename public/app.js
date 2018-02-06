397b9a850ca18f994d55a0fd61da4150a1e1196b
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2018-02-06 12:45:27 -0600

    Scales down properly when changing screen sizes, have to work on X, Y when drawing a line in smaller screen window

--- public/app.js
+++ public/app.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
+
+var socket = io.connect();
+var messageForm = $("#messageForm")
+var message = $("#message")
+var chat = $("#chat")
+var userForm = $("#userForm")
+var userFormArea = $("#userFormArea")
+var messageArea = $("#messageArea")
+var users = $("#users")
+var username = $("#username")
+
 var canvas, ctx,
     brush = {
         x: 0,
         y: 0,
         color: '#000000',
-        size: 10,
+        size: 1,
         down: false,
     },
     strokes = [],
     currentStroke = null;
 
 function redraw () {
+    console.log(strokes)
 
      if ($(window).width() > 1200) {
 
@@ -18,34 +30,9 @@ function redraw () {
         canvas[0].height = 555
         canvas[0].style.width = 1135    
         canvas[0].style.height = 555;
-   
-    }
 
-    else if ($(window).width() <= 1200 && $(window).width() > 991) {
-
-        canvas[0].width = 935
-        canvas[0].height = 460
-        canvas[0].style.width = 935    
-        canvas[0].style.height = 460;
-   
-    }
-
-    else if ($(window).width() <= 991) {
-
-        canvas[0].width = 720
-        canvas[0].height = 355
-        canvas[0].style.width = 720   
-        canvas[0].style.height = 355;
-
-    }
-
-
-    else {
-
-    }
-
-    ctx.clearRect(0, 0, canvas.width(), canvas.height());
-    ctx.lineCap = 'round';
+        ctx.clearRect(0, 0, canvas.width(), canvas.height());
+        ctx.lineCap = 'round';
     for (var i = 0; i < strokes.length; i++) {
         var s = strokes[i];
         ctx.strokeStyle = s.color;
@@ -58,43 +45,96 @@ function redraw () {
         }
         ctx.stroke();
     }
-}
-
-function init () {
-    canvas = $('#draw');
-    ctx = canvas[0].getContext('2d');
-
-    if ($(window).width() > 1200) {
-
-        canvas[0].width = 1135
-        canvas[0].height = 555
-        canvas[0].style.width = 1135    
-        canvas[0].style.height = 555;
+        
    
     }
 
     else if ($(window).width() <= 1200 && $(window).width() > 991) {
 
         canvas[0].width = 935
-        canvas[0].height = 460
+        canvas[0].height = 400
         canvas[0].style.width = 935    
-        canvas[0].style.height = 460;
+        canvas[0].style.height = 400;
+        var theStrokes = [];
+
+        if (strokes.length > 0) {
+            for (var i = 0; i < strokes.length; i++) {
+                theStrokes.push({"color": strokes[i].color, size: strokes[i].size, points: []})
+                for (var j = 0; j < strokes[i].points.length; j++) {
+                    theStrokes[i].points.push({x: strokes[i].points[j].x * canvas[0].width / 1135, y: strokes[i].points[j].y * canvas[0].height / 555})
+                }
+            }
+            ctx.clearRect(0, 0, canvas.width(), canvas.height());
+            ctx.lineCap = 'round';
+        for (var i = 0; i < theStrokes.length; i++) {
+            var s = theStrokes[i];
+            ctx.strokeStyle = s.color;
+            ctx.lineWidth = s.size;
+            ctx.beginPath();
+            ctx.moveTo(s.points[0].x, s.points[0].y);
+        for (var j = 0; j < s.points.length; j++) {
+            var p = s.points[j];
+            ctx.lineTo(p.x, p.y);
+        }
+        ctx.stroke();
+    }
+        }
+        
    
     }
 
-    else if ($(window).width() <= 991) {
+    else if ($(window).width() <= 991 && $(window).width() > 767) {
 
         canvas[0].width = 720
         canvas[0].height = 355
         canvas[0].style.width = 720   
         canvas[0].style.height = 355;
+        
 
     }
-
 
     else {
-
+        
     }
+
+    
+}
+
+function init () {
+    canvas = $('#draw');
+    ctx = canvas[0].getContext('2d');
+
+    // if ($(window).width() > 1200) {
+
+    //     canvas[0].width = 1135
+    //     canvas[0].height = 555
+    //     canvas[0].style.width = 1135    
+    //     canvas[0].style.height = 555;
+   
+    // }
+
+    // else if ($(window).width() <= 1200 && $(window).width() > 991) {
+
+    //     canvas[0].width = 935
+    //     canvas[0].height = 460
+    //     canvas[0].style.width = 935    
+    //     canvas[0].style.height = 460;
+   
+    // }
+
+    // else if ($(window).width() <= 991) {
+
+    //     canvas[0].width = 720
+    //     canvas[0].height = 355
+    //     canvas[0].style.width = 720   
+    //     canvas[0].style.height = 355;
+
+    // }
+
+
+    // else {
+
+    // }
 
         
     
@@ -179,16 +219,6 @@ function init () {
 $(init);
 
 
-	
-	var socket = io.connect();
-	var messageForm = $("#messageForm")
-	var message = $("#message")
-	var chat = $("#chat")
-	var userForm = $("#userForm")
-	var userFormArea = $("#userFormArea")
-	var messageArea = $("#messageArea")
-	var users = $("#users")
-	var username = $("#username")
 
 	messageForm.submit(function(e){
 		e.preventDefault();
